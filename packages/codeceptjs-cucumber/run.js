21f52e63c0155953cbdfbc78e7ace482e89c6975
Kushang Gajjar <kgajjar@salesforce.com>
2020-02-06 12:30:58 -0500

    run

--- packages/codeceptjs-cucumber/run.js
+++ packages/codeceptjs-cucumber/run.js
@@ -1,9 +1,10 @@
 const { event } = require('codeceptjs');
 const Workers = require('codeceptjs/lib/workers');
-const workers = new Workers(4, { by: 'tests' }); // can also be by suite, function etc
+const workers = new Workers(2, { by: 'tests' }); // can also be by suite, function etc
 
 workers.run();
-console.log(JSON.stringify(workers.workers, undefined, 4));
+
+console.log('\n\nworkers.workers: ', workers.workers);
 
 workers.on(event.test.failed, (failedTest, err) => { // use standard constants
   console.log('Failed', failedTest);
